02e2afb39697e79c49607a5284e36a69f2387244
Oluwaseyi <seyi.juliana@gmail.com>
2020-07-17 14:39:16 +0100

    worked on the functionality of the buttons

--- AddToCart/js/custom.js
+++ AddToCart/js/custom.js
@@ -1,6 +1,6 @@
-let counter = document.getElementById("counter");
+let quantity = Array.from(document.getElementsByClassName("quantity"));
+let counter = document.getElementsByClassName("counter");
 let x = parseInt(counter.value);
-
 
 function addValue() {
   if (x < 100) {
@@ -22,51 +22,81 @@ function minusValue() {
   }
 }
 
-//DISPLAY WOMEN AND MEN WHEN BUTTON IS CLICKED
+//DISPLAY WOMEN, MEN AND KIDS WHEN BUTTON IS CLICKED
 let men = [];
 let women = [];
-let gender = Array.from(document.getElementsByClassName('item'));
-console.log(gender)
+let kids = [];
+let gender = Array.from(document.getElementsByClassName("item"));
+console.log(gender);
 
 for (let j = 0; j < document.getElementsByName("men").length; j++) {
   men.push(document.getElementsByName("men")[j]);
 }
 
 for (let i = 0; i < document.getElementsByName("women").length; i++) {
-    women.push(document.getElementsByName("women")[i]);
-  }
+  women.push(document.getElementsByName("women")[i]);
+}
+
+for (let k = 0; k < document.getElementsByName("kids").length; k++) {
+  kids.push(document.getElementsByName("kids")[k]);
+}
 
 function displayWomen() {
   for (let j = 0; j < men.length; j++) {
-      men[j].style.display = "none";
+    men[j].style.display = "none";
+  }
+
+  for (let k = 0; k < kids.length; k++) {
+    kids[k].style.display = "none";
   }
 
   for (let i = 0; i < women.length; i++) {
     women[i].style.display = "flex";
     if (window.innerWidth < 830) {
-        women[i].style.display = "block";
+      women[i].style.display = "block";
     }
-}
+  }
 }
 
 function displayMen() {
   for (let i = 0; i < women.length; i++) {
-      women[i].style.display = "none";
+    women[i].style.display = "none";
+  }
+
+  for (let k = 0; k < kids.length; k++) {
+    kids[k].style.display = "none";
   }
 
   for (let j = 0; j < men.length; j++) {
     men[j].style.display = "flex";
     if (window.innerWidth < 830) {
-        men[j].style.display = "block";
+      men[j].style.display = "block";
     }
+  }
 }
+
+function displayKids() {
+  for (let i = 0; i < women.length; i++) {
+    women[i].style.display = "none";
+  }
+
+  for (let j = 0; j < men.length; j++) {
+    men[j].style.display = "none";
+  }
+
+  for (let k = 0; k < kids.length; k++) {
+    kids[k].style.display = "flex";
+    if (window.innerWidth < 830) {
+      kids[k].style.display = "block";
+    }
+  }
 }
 
 function displayAll() {
-    for (let x = 0; x < gender.length; x++) {
-        gender[x].style.display = "flex";
-        if (window.innerWidth < 830) {
-            gender[x].style.display = "block";
-        }
+  for (let x = 0; x < gender.length; x++) {
+    gender[x].style.display = "flex";
+    if (window.innerWidth < 830) {
+      gender[x].style.display = "block";
     }
+  }
 }
